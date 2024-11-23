fc6deb27cd75c3010e4a62ec9c51e5b6de15a6a9
mamadusama <seu_email@example.commamadusamadev@gmail.com>
2024-11-23 02:46:34 +0000

    feat add new version

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 from os import path
 
-# Lê a descrição longa do arquivo README.md
+# Aqui estamos pegando o caminho da pasta onde o setup.py está localizado
 here = path.abspath(path.dirname(__file__))
+
+# Abre o README.md na mesma pasta do setup.py
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name="ms_toolkit",
-    version="0.1.0",
-    packages=find_packages(),  # Encontra pacotes automaticamente
+    version="0.1.2",
+    packages=find_packages(),
     install_requires=[
         "pandas",
         "numpy",
@@ -18,8 +20,8 @@ setup(
         "scipy",
     ],
     description="Pacote para manipulação, limpeza e visualização de dados.",
-    long_description=long_description,  # Adiciona a descrição longa do README.md
-    long_description_content_type="text/markdown",  # Define o tipo de conteúdo como Markdown
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Garantir que é markdown
     author="Mamadu Sama",
     author_email="mamadusama19@gmail.com",
     url="https://github.com/mamadusama/ms_toolkit",
@@ -29,5 +31,5 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only",
     ],
-    python_requires='>=3.6',  # Define a versão mínima do Python
+    python_requires='>=3.6',
 )
